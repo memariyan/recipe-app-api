92dfca1b01b2e2c3f981915c3fd142b121ee65ed
Mohammad Memariyan <memariyan74@gmail.com>
2024-12-25 11:00:07 +0330

    feat: admin dashboard support

--- app/core/models.py
+++ app/core/models.py
@@ -8,22 +8,22 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None):
-        user = self.set_base_user_data(email, password)
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.set_base_user_data(email, password, **extra_fields)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.set_base_user_data(email, password)
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.set_base_user_data(email, password, **extra_fields)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
 
-    def set_base_user_data(self, email, password):
+    def set_base_user_data(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         return user
 
